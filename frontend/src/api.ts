314672be444757c49ec851f39cad8c0fa9a63b16
pawel-laptop <p.blaszczy@gmail.com>
2025-03-17 19:38:02 +0100

    Random change to  trigger workflow

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -3,7 +3,7 @@ import { FormData } from "@/types/form";
 export async function getCities(): Promise<string[]> {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   const response: { [key: string]: string[] } = {
-    cities: ["Kraków", "Warszawa", "Wrocław", "Poznań", "Gdańsk"],
+    cities: ["Kraków", "Warszawa", "Wrocław", "Poznań", "Gdańsk", "Łódź"],
   };
   return response.cities;
 }
@@ -16,6 +16,7 @@ export async function getDistricts(city: string): Promise<string[]> {
     Wrocław: ["Stare Miasto", "Krzyki", "Fabryczna", "Psie Pole"],
     Poznań: ["Stare Miasto", "Jeżyce", "Wilda", "Grunwald"],
     Gdańsk: ["Stare Miasto", "Wrzeszcz", "Orunia", "Jasień"],
+    Łódź: ["Bałuty", "Polesie", "Widzew", "Górna"],
   };
   // @ts-ignore
   return city in response ? response[city] : [];
