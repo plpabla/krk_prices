a0a72c239d7e6fb9708f39018712648f4d378bbf
pawel-laptop <p.blaszczy@gmail.com>
2025-03-24 15:27:32 +0100

    Add router
    Start working on #47

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+
 import FlatEstimationForm from "@/components/FlatEstimationForm";
 import Footer from "@/components/Footer";
+import Navbar from "./components/Navbar";
 
 const theme = createTheme({
   palette: {
@@ -14,7 +17,11 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <FlatEstimationForm />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<FlatEstimationForm />} />
+        <Route path="/demo" element={<h1>test</h1>} />
+      </Routes>
       <Footer />
     </ThemeProvider>
   );
