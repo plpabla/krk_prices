29d6e40de7b0f6d12cc137c5e9413abaa31558a2
pawel-laptop <p.blaszczy@gmail.com>
2025-03-24 16:07:21 +0100

    add context manager

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from routes import estimate
 
-app = FastAPI(title="House Price Estimator", version="0.1.0")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # any preprocessing here
+    print(">>> Starting the app")
+    yield
+    # any cleanup here
+    print(">>> Stopping the app")
+
+
+app = FastAPI(title="House Price Estimator", version="0.1.0", lifespan=lifespan)
 app.include_router(estimate.router)
 
 
