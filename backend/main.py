9c63f1a358646d269a3d76ddf2592445251ad35b
pawel-laptop <p.blaszczy@gmail.com>
2025-03-25 09:58:33 +0100

    add cities and district endpoints
    create initial schema for that. I'll need it later on for DB integration

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
-from routes import estimate
+from routes import estimate, cities
 
 
 @asynccontextmanager
@@ -15,9 +15,10 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="House Price Estimator", version="0.1.0", lifespan=lifespan)
 app.include_router(estimate.router)
+app.include_router(cities.router)
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app="main:app", host="localhost", port=8001, reload=True)
---
+++ backend/routes/cities.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter
+
+from schemas.city import City
+from schemas.district import District, DistrictOutput
+
+router = APIRouter(prefix="/cities")
+
+
+@router.get("/")
+async def get_cities() -> list[City]:
+    return [
+        City(name="Krakow"),
+        City(name="Warszawa"),
+        City(name="Wroclaw"),
+        City(name="Gdańsk"),
+    ]
+
+
+@router.get("/{city_name}")
+async def get_city(city_name: str) -> list[DistrictOutput]:
+    city = City(name=city_name)
+    return [
+        DistrictOutput.model_validate(
+            District(city=city, name="Stare Miasto").model_dump()
+        ),
+        DistrictOutput.model_validate(
+            District(city=city, name="Krowodrza").model_dump()
+        ),
+        DistrictOutput.model_validate(
+            District(city=city, name="Podgórze").model_dump()
+        ),
+    ]
