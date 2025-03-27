af5efc9a45437206d97a94d5d355a6513f9e9e51
pawel-laptop <p.blaszczy@gmail.com>
2025-03-27 15:38:21 +0100

    change filenames to be used and reformat code

--- model/src/train_model.py
+++ model/src/train_model.py
@@ -5,17 +5,23 @@ from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
 
 # Wczytanie danych
-train_data = pd.read_csv('../../data/otodom_train.csv')
-test_data = pd.read_csv('../../data/otodom_test.csv')
+train_data = pd.read_csv("../data/otodom_district_train.csv")
+test_data = pd.read_csv("../data/otodom_district_test.csv")
 
 # Wybór cech i zmiennej docelowej
-X_train = train_data.drop(columns=['price'])  # Załóżmy, że przewidujemy 'price'
-y_train = train_data['price']
-X_test = test_data.drop(columns=['price'])
-y_test = test_data['price']
+X_train = train_data.drop(columns=["price"])  # Załóżmy, że przewidujemy 'price'
+y_train = train_data["price"]
+X_test = test_data.drop(columns=["price"])
+y_test = test_data["price"]
 
 # Inicjalizacja modelu
-model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=10000, learning_rate=0.01, max_depth=2, random_state=42)
+model = xgb.XGBRegressor(
+    objective="reg:squarederror",
+    n_estimators=10000,
+    learning_rate=0.01,
+    max_depth=2,
+    random_state=42,
+)
 
 # Trenowanie modelu
 model.fit(X_train, y_train)
@@ -28,12 +34,12 @@ mse = mean_squared_error(y_test, predictions)
 rmse = np.sqrt(mse)
 r2 = r2_score(y_test, predictions)
 
-print(f'Mean Squared Error (MSE): {mse}')
-print(f'Root Mean Squared Error (RMSE): {rmse}')
-print(f'R-squared (R²): {r2}')
+print(f"Mean Squared Error (MSE): {mse}")
+print(f"Root Mean Squared Error (RMSE): {rmse}")
+print(f"R-squared (R²): {r2}")
 
 # Zapisanie modelu do pliku
-with open('../out/xgboost_model.pkl', 'wb') as file:
+with open("../out/xgboost_model.pkl", "wb") as file:
     pickle.dump(model, file)
 
 print("✅ Model XGBoost wytrenowany i zapisany!")
