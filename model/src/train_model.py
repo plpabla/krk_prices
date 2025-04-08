d925ba8395f61e8642618136c4fe5d8464eb087d
pawel-laptop <p.blaszczy@gmail.com>
2025-04-08 19:21:43 +0200

    check categories instead of one-hot encoding

--- model/src/train_model.py
+++ model/src/train_model.py
@@ -6,6 +6,23 @@ from sklearn.model_selection import GridSearchCV
 import numpy as np
 
 
+def convert_str_to_category(
+    train: pd.DataFrame, test: pd.DataFrame
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    # Przekonwertuj kolumny kategoryczne na Categorical
+    for col in train.select_dtypes(include=["object"]).columns:
+        train[col] = train[col].astype("category")
+    for col in test.select_dtypes(include=["object"]).columns:
+        test[col] = test[col].astype("category")
+    # Upewnij się, że kolumny kategoryczne mają te same kategorie w zbiorach treningowych i testowych
+    for col in train.select_dtypes(include=["category"]).columns:
+        test[col] = test[col].cat.set_categories(train[col].cat.categories)
+    # Upewnij się, że kolumny kategoryczne mają te same kategorie w zbiorach treningowych i testowych
+    for col in test.select_dtypes(include=["category"]).columns:
+        train[col] = train[col].cat.set_categories(test[col].cat.categories)
+    return train, test
+
+
 def run():
     # Wczytanie danych
     train_data = pd.read_csv("../data/otodom_district_train.csv")
@@ -19,12 +36,14 @@ def run():
     X_test = test_data.drop(columns=["price", "price_m2"])
     y_test = test_data["price"]
 
+    X_train, X_test = convert_str_to_category(X_train, X_test)
+
     params_grid = {
-        "max_depth": [12],
+        "max_depth": [8, 12],
         "learning_rate": [0.005],
-        "n_estimators": [20_000],
+        "n_estimators": [10_000, 20_000],
         "subsample": [0.3],
-        "colsample_bytree": [0.8],
+        "colsample_bytree": [0.8, 1.0],
     }
 
     # Inicjalizacja modelu
@@ -34,6 +53,7 @@ def run():
         learning_rate=0.01,
         max_depth=8,
         random_state=42,
+        enable_categorical=True,
     )
 
     model_grid = GridSearchCV(
