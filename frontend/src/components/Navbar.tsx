7b2967cf2e5ffbc1771f425ddc4568cda9ed98ae
pawel-laptop <p.blaszczy@gmail.com>
2025-05-04 17:31:41 +0200

    Apply styles for a navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,18 @@
+import "./Navbar.css"; // Import the CSS file for styling
+
 export default function Navbar() {
   return (
     <nav>
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li>
+          <a href="#">Wycena</a>
+        </li>
+        <li>
+          <a href="#">Analiza zdjęć</a>
+        </li>
+        <li>
+          <a href="#">Kontakt</a>
+        </li>
       </ul>
     </nav>
   );
