537035944783a280f95b0231b5e25c628c02ccdf
pawel-laptop <p.blaszczy@gmail.com>
2025-05-05 20:08:12 +0200

    add photo upload with displaying preview

--- frontend/src/components/PhotoUpload.tsx
+++ frontend/src/components/PhotoUpload.tsx
@@ -1,9 +1,57 @@
+import { useRef, useState } from "react";
+
 export default function PhotoUpload() {
+  const [imgSrc, setImgSrc] = useState<string>("");
+  const fileEl = useRef<HTMLInputElement>(null);
+
+  const displayFile = (file: File) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setImgSrc(reader.result as string);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const submit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (fileEl.current && fileEl.current.files) {
+      const file = fileEl.current.files[0];
+      const formData = new FormData();
+      formData.append("file", file);
+
+      fetch("http://localhost:8000/upload", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+  };
+
   return (
     <div>
       <h1>Wczytaj zdjęcie</h1>
-      <input type="file" accept="image/*" />
-      <button type="submit">Wczytaj</button>
+      <form onSubmit={submit}>
+        <input
+          type="file"
+          accept="image/*"
+          ref={fileEl}
+          onChange={(e) => {
+            if (e.target.files) {
+              displayFile(e.target.files[0]);
+            }
+          }}
+        />
+        <div>
+          <img src={imgSrc} />
+        </div>
+        <button type="submit">Wyślij</button>
+      </form>
     </div>
   );
 }
