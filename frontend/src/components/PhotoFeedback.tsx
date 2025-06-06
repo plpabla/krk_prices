0b26ca173f9e57f0e4449b7dd2617d2c4be6a8b7
pawel-laptop <p.blaszczy@gmail.com>
2025-06-06 18:51:14 +0200

    render differently for photoFeedback==0 (no photos submitted)

--- frontend/src/components/PhotoFeedback.tsx
+++ frontend/src/components/PhotoFeedback.tsx
@@ -7,31 +7,41 @@ export default function PhotoFeedback({
   toFix,
   description,
 }: PhotoFeedbackProps) {
+  const photosFeedback = luxuryLevel != 0;
+
   return (
     <>
-      <h2>Analiza zdjęcia - poziom atrakcyjności: {luxuryLevel}/10</h2>
-      <p>{luxuryReason}</p>
-      <div>
-        <h3>Co jest dobrze?</h3>
-        <ul>
-          {pros.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-        <h3>Co można poprawić?</h3>
-        <ul>
-          {toFix.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-
-        {description && (
+      <h2>
+        {photosFeedback
+          ? `Analiza zdjęcia - poziom atrakcyjności: ${luxuryLevel}/10`
+          : ""}
+      </h2>
+      {photosFeedback && (
+        <>
+          <p>{luxuryReason}</p>
           <div>
-            <h3>Sugerowany opis</h3>
-            <p>{description}</p>
+            <h3>Co jest dobrze?</h3>
+            <ul>
+              {pros.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+            <h3>Co można poprawić?</h3>
+            <ul>
+              {toFix.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
           </div>
-        )}
-      </div>
+        </>
+      )}
+
+      {description && (
+        <div>
+          <h3>Sugerowany opis</h3>
+          <p>{description}</p>
+        </div>
+      )}
     </>
   );
 }
